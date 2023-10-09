85d2b059eff25887b169bef587e0d3412bb619c6
Joel Gallardo Manjarrez <joelgallardomn@gmail.com>
2023-10-08 18:25:17 -0700

    add datos al state con ref y reactive

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
+import { ref, reactive, onMounted } from 'vue'
+import { db } from './data/guitarras'
+
+// const state = reactive({    //si son objetos datos relacionados utilizar reactive
+//     guitarras: []
+// });
+// console.log(state.guitarras);
+
+const guitarras = ref([]);    //si son otros tipos de datos ejem bool string etc utilizar ref
+
+onMounted(() => {//metodo que se ejecuta cuando el componente esta montado 
+    guitarras.value = db;//aqui podria hacer un get a una api con fetch para traer los datos
+    //state.guitarras = db;
+});
 
 </script>
 
 <template>
-
-  <header class="py-5 header">
+    <header class="py-5 header">
         <div class="container-xl">
             <div class="row justify-content-center justify-content-md-between">
                 <div class="col-8 col-md-3">
@@ -13,9 +26,7 @@
                     </a>
                 </div>
                 <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                    <div 
-                        class="carrito"
-                    >
+                    <div class="carrito">
                         <img class="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" class="bg-white p-3">
@@ -37,28 +48,19 @@
                                         </td>
                                         <td>SRV</td>
                                         <td class="fw-bold">
-                                                $299
+                                            $299
                                         </td>
                                         <td class="flex align-items-start gap-4">
-                                            <button
-                                                type="button"
-                                                class="btn btn-dark"
-                                            >
+                                            <button type="button" class="btn btn-dark">
                                                 -
                                             </button>
-                                                1
-                                            <button
-                                                type="button"
-                                                class="btn btn-dark"
-                                            >
+                                            1
+                                            <button type="button" class="btn btn-dark">
                                                 +
                                             </button>
                                         </td>
                                         <td>
-                                            <button
-                                                class="btn btn-danger"
-                                                type="button"
-                                            >
+                                            <button class="btn btn-danger" type="button">
                                                 X
                                             </button>
                                         </td>
@@ -76,12 +78,11 @@
             <div class="row mt-5">
                 <div class="col-md-6 text-center text-md-start pt-5">
                     <h1 class="display-2 fw-bold">Modelo VAI</h1>
-                    <p class="mt-5 fs-5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, possimus quibusdam dolor nemo velit quo, fuga omnis, iure molestias optio tempore sint at ipsa dolorum odio exercitationem eos inventore odit.</p>
+                    <p class="mt-5 fs-5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+                        possimus quibusdam dolor nemo velit quo, fuga omnis, iure molestias optio tempore sint at ipsa
+                        dolorum odio exercitationem eos inventore odit.</p>
                     <p class="text-primary fs-1 fw-black">$399</p>
-                    <button 
-                        type="button"
-                        class="btn fs-4 bg-primary text-white py-2 px-5"
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn fs-4 bg-primary text-white py-2 px-5">Agregar al Carrito</button>
                 </div>
             </div>
         </div>
@@ -99,12 +100,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
 
@@ -114,12 +113,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">SRV</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
 
@@ -129,12 +126,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">Borland</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
 
@@ -144,12 +139,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">Vai</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
 
@@ -159,12 +152,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">Thompson</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
 
@@ -174,12 +165,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">White</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
 
@@ -189,12 +178,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">Cobain</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
             <div class="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -203,12 +190,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">Dale</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
             <div class="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -217,12 +202,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">Krieger</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
             <div class="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -231,12 +214,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">Campbell</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
             <div class="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -245,12 +226,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">Reed</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
             <div class="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -259,12 +238,10 @@
                 </div>
                 <div class="col-8">
                     <h3 class="text-black fs-4 fw-bold text-uppercase">Hazel</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
+                        nesciunt, amet deserunt</p>
                     <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
+                    <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
                 </div>
             </div><!-- FIN GUITARRA -->
         </div>
@@ -276,5 +253,4 @@
             <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
         </div>
     </footer>
-
 </template>
