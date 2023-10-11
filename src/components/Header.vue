bc589d7f819c91f70fc6ac165da38c2108979207
Joel Gallardo Manjarrez <joelgallardomn@gmail.com>
2023-10-10 22:54:48 -0700

    mostrando el contenido del carrito

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -35,19 +35,20 @@ const props = defineProps({
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
+                                        <tr v-for="producto in carrito">
                                             <td>
-                                                <img class="img-fluid" src="/img/guitarra_02.jpg" alt="imagen guitarra">
+                                                <img class="img-fluid" :src="'/img/' + producto.imagen + '.jpg'"
+                                                    :alt="'imagen guitarra' + producto.nombre">
                                             </td>
-                                            <td>SRV</td>
+                                            <td>{{ producto.nombre }}</td>
                                             <td class="fw-bold">
-                                                $299
+                                                ${{ producto.precio }}
                                             </td>
                                             <td class="flex align-items-start gap-4">
                                                 <button type="button" class="btn btn-dark">
                                                     -
                                                 </button>
-                                                1
+                                                {{ producto.cantidad }}
                                                 <button type="button" class="btn btn-dark">
                                                     +
                                                 </button>
